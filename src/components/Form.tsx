f4ddd038f70c86b9861c6205d6151d84aa85dd48
Cyber-Sam33 <issasammi@yahoo.com>
2023-04-08 12:21:44 -0400

    Disable button if invalid vlues entered

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { number, z } from "zod";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
@@ -16,7 +16,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -47,7 +47,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
