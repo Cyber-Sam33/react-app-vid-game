dc1a7d185e259005d2608d07698cff1fba413411
Cyber-Sam33 <issasammi@yahoo.com>
2023-04-10 13:26:30 -0400

    add expense-tracker and ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,46 @@ import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Form from "./components/Form";
 import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-  const [alertVisble, setAlertVisibility] = useState(false);
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "aaa",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 2,
+      description: "bbb",
+      amount: 20,
+      category: "Utilities",
+    },
+    {
+      id: 3,
+      description: "ccc",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 4,
+      description: "ddd",
+      amount: 10,
+      category: "Utilities",
+    },
+  ]);
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+  // const [alertVisble, setAlertVisibility] = useState(false);
   return (
     <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
       {/* <ListGroup
         items={items}
         heading="Cities"
@@ -23,7 +54,7 @@ function App() {
       <Button colour="primary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button> */}
-      <Form />
+      {/* <Form /> */}
     </div>
   );
 }
